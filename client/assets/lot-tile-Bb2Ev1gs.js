d4a543ec2f8d0213494b48ebbfad6cef44aa375a
mutcontato-cyber <mutcontato-cyber@users.noreply.github.com>
2026-06-25 17:10:49 +0000

    deploy: 902386ff17c35a2f28c9c92039df6b648f5d8b87

--- client/assets/lot-tile-Bb2Ev1gs.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./useRouter-BeG5N71-.js";e();var t={sem_cadastro:`Sem cadastro`,cadastrado:`Cadastrado`,incompleto:`Cadastro incompleto`,confirmado:`Confirmou apoio`,pendencia:`Pendência`};export{t};
--- client/assets/route-BW-BrmQL.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./useRouter-BeG5N71-.js";import{i as t}from"./index-Dfh7e254.js";var n=e(),r=()=>(0,n.jsx)(t,{});export{r as component};
